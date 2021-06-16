11bc42d2f655da163cecc1113328a40614915cb8
Bettina Lengger <bettina.lengger@gmail.com>
2021-06-16 13:32:46 +0200

    summary statistics

--- R/wrangling-session.R
+++ R/wrangling-session.R
@@ -137,6 +137,28 @@ nhanes_update <- nhanes_small %>%
 str(nhanes_update)
 
 
+## Summary statistics by group
+
+nhanes_small %>%
+    summarise(max_bmi = max(bmi))
+
+nhanes_small %>%
+    summarise(max_bmi = max(bmi, na.rm = TRUE))
+
+# find out how many missing values (NAs) are there
+nhanes_small %>%
+    summarise(sum_na = sum(is.na(bmi)))
+
+# calculating 2 summary statistics
+nhanes_small %>%
+    summarise(max_bmi = max(bmi, na.rm = TRUE),
+              min_bmi = min(bmi, na.rm = TRUE))
+
+
+
+
+
+
 
 
 
